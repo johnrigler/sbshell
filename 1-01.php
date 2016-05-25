ea22799f831c284ee4bbc1addf681cdeb1a70a61
john <john@secretbeachsolutions.com>
2016-05-24 22:40:44 -0500

    modified:   1-01.php

--- 1-01.php
+++ 1-01.php
@@ -1,4 +1,6 @@
   <div>
-Welcome to sbshell. This subdirectory is a self-contained documentation solution that is easy to install and configure.  If you are reading this, then you have already installed it into php and drilled down to the sbshell directory. You are now looking at the sbshell interface and using it to see both the structure of the system and its documentation.  The next step is to cut and paste the text below into a text file on a remote server and name it 'sbshell', then you can begin using the tool.
-
+Welcome to sbshell. This subdirectory is a self-contained documentation solution that is easy to install and configure.  If you are reading this, then you have already installed it into php and drilled down to the sbshell directory. You are now looking at the sbshell interface and using it to see both the structure of the system and its documentation.  
+<p>
+You will find a bash library in this directory called 'sbshell', link it to /usrlocal/bin/sbshell (or wherever else that you prefer, put it in your path,  and dot it into your shell when you want to use it.
+</p>
   </div>
