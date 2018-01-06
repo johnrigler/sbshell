7d888d1bdd68b1868566c3d8c6c31e4fe729c5e8
John Rigler <john@rigler.org>
2018-01-05 22:15:01 -0600

    added color

--- lib/sb/shell.bash
+++ lib/sb/shell.bash
@@ -1,9 +1,10 @@
-__sb.sb.shell() {
+__sb.sb.shell () 
+{ 
+    _SBFILE=sb.bash;
+    _SBCOLOR=white;
+    _SBDIR=/usr/local/sbshell/;
+    _SBLIB=/usr/local/sbshell/sb.bash;
+    color white
+}
 
-_SBFILE=sb.bash
-_SBCOLOR=white
-_SBDIR=/usr/local/sbshell/
-_SBLIB=/usr/local/sbshell/sb.bash
-
-} ; _sb.init 'sb.shell'
-
+_sb.init sb.shell
