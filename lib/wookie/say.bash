eacdf11536b7a19958f49ac85087510e15aa0fa7
John Rigler <john@rigler.org>
2018-01-09 10:13:10 -0600

    wookie.church

---
+++ lib/wookie/say.bash
@@ -0,0 +1,11 @@
+__sb.wookie.say () 
+{ 
+    - Imagine yourself at a party and you say;
+    -- maybe I should create my own damn religion;
+    - Imagine that this is that religion;
+    - I am a nerd that broads falls into the category of None;
+    - It is about wookies and laser cutters and bitcoin and all the other stuff that you would try to cram it;
+    -- Becauses now is the time to reimagine how we all can be together and do something useful.
+}
+
+_sb.init wookie.say
